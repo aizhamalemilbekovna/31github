4fae7fbbf3a1bb9747466b1ca6891adf29e1a071
Aizhamal <84260323+aizhamalemilbekovna@users.noreply.github.com>
2023-12-04 23:23:17 +0600

    Add files via upload

---
+++ Practice-3/script.js
@@ -0,0 +1,49 @@
+// 3-day
+
+// 1. Создайте переменную с именем "age" и присвойте ей свой возраст. Выведите значение этой переменной.
+let age = 20;
+console.log(age);
+
+// 2. Создайте переменные "num1" и "num2", присвойте им числовые значения, затем сложите их и выведите результат.
+let num1 = 8;
+let num2 = 4;
+let result = num1 + num2;
+console.log(result);
+
+// 3. Создайте переменную "greeting" и присвойте ей строковое значение приветствия. Выведите это приветствие.
+let greeting = "Привет, мир!";
+console.log(greeting);
+
+// 4. Создайте переменную "isStudent" и присвойте ей логическое значение (true/false) в зависимости от того, студент вы или нет. Выведите это значение.
+let isStudent = true;
+console.log(isStudent);
+
+// 5. Создайте переменную "firstNumber" и присвойте ей числовое значение. Затем создайте вторую переменную "secondNumber" и присвойте ей значение, равное удвоенному значению "firstNumber". Выведите обе переменные.
+let firstNumber = 6;
+let secondNumber = firstNumber * 2;
+console.log(firstNumber);
+console.log(secondNumber);
+
+// 6. Создайте переменную "fullName" и присвойте ей ваше полное имя. Выведите значение этой переменной.
+let fullName = "Aizhamal Bektursunova";
+console.log(fullName);
+
+// 7. Создайте переменные "width" и "height" и присвойте им числовые значения, представляющие ширину и высоту прямоугольника. Затем вычислите площадь прямоугольника (произведение ширины на высоту) и выведите результат.
+let width = 10;
+let height = 5;
+let rectangleArea = width * height;
+console.log(rectangleArea);
+
+// 8. Создайте переменную "circleRadius" и присвойте ей числовое значение радиуса круга. Затем вычислите площадь круга (пи умножить на квадрат радиуса) и выведите результат.
+let circleRadius = 7;
+let circleArea = Math.PI * Math.pow(circleRadius, 2);
+console.log(circleArea);
+
+// 9. Создайте переменную "temperatureCelsius" и присвойте ей числовое значение температуры в градусах Цельсия. Затем вычислите температуру в градусах Фаренгейта по формуле (C * 9/5) + 32 и выведите результат.
+let temperatureCelsius = 25;
+let temperatureFahrenheit = (temperatureCelsius * 9) / 5 + 32;
+console.log(temperatureFahrenheit);
+
+// 10. Создайте переменную "daysInMonth" и присвойте ей число, представляющее количество дней в месяце (например, 30 или 31). Выведите сообщение: "В месяце [daysInMonth] дней."
+let daysInMonth = 31;
+console.log("В месяце " + daysInMonth + " дней.");
